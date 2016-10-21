18275ecf949cb440835a9404a6c77e727137e440
Adam Zinder <adam.zinder@gmail.com>
2016-10-21 10:29:19 -0700

    display text due to debug

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 //==============BL=========================
 
-function yourTrack (question1, question2, question3, question4, question5, question6);{
+function yourTrack (question1, question2, question3, question4, question5, question6) {
   var sum = question1 + question2 + question3 + question4 + question5 + question6;
 
-  if (sum >=6 && sum < 13) {
-    return "CSS-Desisgn";
+  if (sum >=6 && sum < 14) {
+    return "CSS-Design";
   }
-  else if (sum >=13 && sum <19) {
+  else if (sum >= 14 && sum <19) {
     return "Ruby-Rails";
   }
-  else if(sum >=19 && sum<=24) {
+  else if(sum >= 19 && sum <= 24) {
     return "C-NET"
   }
 };
@@ -21,7 +21,7 @@ function yourTrack (question1, question2, question3, question4, question5, quest
 
 
 //=================UI====================
-(document).ready(function(){})
+$(document).ready(function() {
   $('#quiz form').submit(function(event){
     var question1 = parseInt($("input:radio[name=question1]:checked").val());
     var question2 = parseInt($("input:radio[name=question2]:checked").val());
@@ -29,7 +29,7 @@ function yourTrack (question1, question2, question3, question4, question5, quest
     var question4 = parseInt($("input:radio[name=question4]:checked").val());
     var question5 = parseInt($("input:radio[name=question5]:checked").val());
     var question6 = parseInt($("input:radio[name=question6]:checked").val());
-    $("#result").text(yourTrack(question1, question2, question3, question4, question5, question6));
     event.preventDefault();
+    $ ("#result").text(yourTrack(question1, question2, question3, question4, question5, question6));
   });
 });
