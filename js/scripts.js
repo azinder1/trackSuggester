913642c64d02041ea26f5d45e19354b27bfd1875
Adam Zinder <adam.zinder@gmail.com>
2016-10-21 10:09:16 -0700

    use values set in html in jquery function

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 //==============BL=========================
 
-function yourTrack (name1, name2, name3, name4, name5, name6;)
-  var sum = name1 + name2 + name3 + name4 + name5 + name6;
+function yourTrack (question1, question2, question3, question4, question5, question6);{
+  var sum = question1 + question2 + question3 + question4 + question5 + question6;
+
+  if (sum >=6 && sum < 13) {
+    return "CSS-Desisgn";
+  }
+  else if (sum >=13 && sum <19) {
+    return "Ruby-Rails";
+  }
+  else if(sum >=19 && sum<=24) {
+    return "C-NET"
+  }
+};
+
+
+
 
 
 
@@ -9,12 +23,13 @@ function yourTrack (name1, name2, name3, name4, name5, name6;)
 //=================UI====================
 (document).ready(function(){})
   $('#quiz form').submit(function(event){
-    var name1 = parseInt($("input:radio[name]:checked").val());
-    var name2 = parseInt($("input:radio[name=]:checked").val());
-    var name3 = parseInt($("input:radio[name=]:checked").val());
-    var name4 = parseInt($("input:radio[name=]:checked").val());
-    var name5 = parseInt($("input:radio[name=]:checked").val());
-    var name6 = parseInt($("input:radio[name=]:checked").val());
-  event.preventDefault();
+    var question1 = parseInt($("input:radio[name=question1]:checked").val());
+    var question2 = parseInt($("input:radio[name=question2]:checked").val());
+    var question3 = parseInt($("input:radio[name=question3]:checked").val());
+    var question4 = parseInt($("input:radio[name=question4]:checked").val());
+    var question5 = parseInt($("input:radio[name=question5]:checked").val());
+    var question6 = parseInt($("input:radio[name=question6]:checked").val());
+    $("#result").text(yourTrack(question1, question2, question3, question4, question5, question6));
+    event.preventDefault();
   });
 });
